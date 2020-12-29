fca4dff35dc79502f9954bd1c7125c5cd93210a5
Simon Garrelou <simon.garrelou@gmail.com>
2020-12-29 17:51:38 +0100

    Convert Image to grayscale before displaying

--- kindle4/image.go
+++ kindle4/image.go
@@ -11,6 +11,14 @@ import (
 
 // DrawImage write the given Image to the screen at position 0, 0
 func DrawImage(img image.Image) error {
+	// Convert the Image to grayscale
+	grayImg := image.NewGray(img.Bounds())
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			grayImg.Set(x, y, img.At(x, y))
+		}
+	}
+
 	tmp, err := ioutil.TempFile("", "*.png")
 	if err != nil {
 		return err
@@ -18,7 +26,7 @@ func DrawImage(img image.Image) error {
 	defer os.Remove(tmp.Name())
 	defer tmp.Close()
 
-	err = png.Encode(tmp, img)
+	err = png.Encode(tmp, grayImg)
 	if err != nil {
 		return err
 	}
